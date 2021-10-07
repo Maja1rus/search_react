4b68d16ae69d7be687588e21c238ed34543bda71
Maja1rus <nalimoff.deniss@gmail.com>
2021-10-08 01:12:40 +0300

    update-3

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,47 +1,62 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef, useEffect, useCallback} from 'react'
 
-const Dropdown = ({ options, id, label, rgb, prompt, value, onChange }) => {
-    
-    const [open, setOpen] = useState(false);
-    const [query, setQuery] = useState("")
-    const ref = useRef(null);
+const Dropdown = ({options, id, label, rgb, prompt, value, onChange}) => {
+    const [open, setOpen] = useState(false)
+    const [query, setQuery] = useState('')
+    const dropdownEl = useRef()
+
+    // Hiding dropdown
+    const close = useCallback(
+        (e) => {
+            if (
+                open &&
+                e.target.closest('.dropdown') !== dropdownEl.current
+            ) {
+                setOpen(false)
+            }
+        },
+        [open, setOpen, dropdownEl]
+    )
 
     useEffect(() => {
-        document.addEventListener("click", close)
-        return () => document.removeEventListener("click", close)
-    }, [])
-
-    function close(e) {
-        setOpen(e && e.target === ref.current)
-    }
+        document.addEventListener('click', close)
+        return () => document.removeEventListener('click', close)
+    }, [close])
 
     function filter(options) {
-        return options.filter(option => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1);
+        return options.filter(
+            (option) =>
+                option[label].toLowerCase().indexOf(query.toLowerCase()) > -1
+        )
     }
 
     function displayValue() {
-        if (query.length > 0) return query;
-        if (value) return value[label];
-        return "";
+        if (query.length > 0) return query
+        if (value) return value[label]
+        return ''
     }
 
     return (
-        <div className="dropdown">
-            <div className="control" onClick={() => setOpen((prev) => !prev)}>
+        <div
+            className="dropdown">
+            <div className="control">
                 <div className="selected-value">
                     <input
                         type="text"
-                        ref={ref}
                         placeholder={value ? value[label] : prompt}
+                        ref={dropdownEl}
                         value={displayValue()}
                         onChange={(e) => {
                             setQuery(e.target.value)
                             onChange(null)
                         }}
-                        onClick={() => setOpen((prev) => !prev)}
+                        onClick={() => setOpen(!open)}
                     />
                 </div>
-                <div className={`arrow ${open ? 'open' : null}`} />
+                <div
+                    className={`arrow ${open ? 'open' : null}`}
+                    onClick={() => setOpen(!open)}
+                />
             </div>
             <div className={`options ${open ? 'open' : null}`}>
                 {filter(options).map((option) => (
@@ -59,11 +74,7 @@ const Dropdown = ({ options, id, label, rgb, prompt, value, onChange }) => {
                     >
                         <div className="select-wrapper">
                             {option[label]}
-                            <div
-                                className="box-color"
-                                style={{background: `rgb(${option[rgb]})`}}
-                            />
-                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
